37c0785f0f43d14cae1eb319c5216485aca25b88
Crowdbotics <team@crowdbotics.com>
2021-08-03 15:24:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "falling_mud_29403Identifier",
+  url: "https://falling_mud_29403Identifier.botics.co",
+  api: "https://falling_mud_29403Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
